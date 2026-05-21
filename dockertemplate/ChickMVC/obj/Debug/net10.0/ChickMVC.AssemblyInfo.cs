24c7f152863b6fd1e0445040bf071d2ce0d3b41b
jayden-leig <ST10315068@vcconnect.edu.za>
2026-05-21 10:46:10 +0200

    added

--- dockertemplate/ChickMVC/obj/Debug/net10.0/ChickMVC.AssemblyInfo.cs
+++ dockertemplate/ChickMVC/obj/Debug/net10.0/ChickMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChickMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380d3266400d5427e60ad80e11c70dc9777a602d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2eec93d0dc228bcdb83f7c5011e0c3e49d41446")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChickMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChickMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
